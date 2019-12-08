ca0483c6546a3efdc43eecc3c44c2cfa898cc289
mikhail <mmurzin1996@mail.ru>
2019-12-09 00:48:29 +0500

    Config Spring Security

--- src/main/java/sia/tacocloud/config/SecurityConfig.java
+++ src/main/java/sia/tacocloud/config/SecurityConfig.java
@@ -1,22 +1,45 @@
 package sia.tacocloud.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    //@Qualifier("userRepositoryUserDetailsService") annotation can be used here or @Named
+    private UserDetailsService userRepositoryUserDetailsService;
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("buzz")
-                .password("{noop}infinity")
-                .authorities("ROLE_USER")
-                .and()
-                .withUser("woody")
-                .password("{noop}bullseye")
-                .authorities("ROLE_USER");
+        auth.userDetailsService(userRepositoryUserDetailsService).passwordEncoder(encoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .hasRole("USER")
+                .antMatchers("/", "/**").permitAll()
+                 .and()
+                .formLogin()
+                .loginPage("/login")
+                .and().logout()
+                .logoutSuccessUrl("/")
+                .and().csrf().ignoringAntMatchers("/h2-console/**")//don't apply CSRF protection to /h2-console
+                .and().headers().frameOptions().sameOrigin();//allow use of frame to same origin urls;
     }
 }
--- src/main/java/sia/tacocloud/domain/Order.java
+++ src/main/java/sia/tacocloud/domain/Order.java
@@ -39,6 +39,8 @@ public class Order implements Serializable {
     private String ccExpiration;
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
+    @ManyToOne
+    private User user;
 
     @ManyToMany(targetEntity=Taco.class)
     private List<Taco> tacos = new ArrayList<>();
