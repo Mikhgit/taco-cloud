5d13a843361ac9a589bae6c3eb62d465c36629e1
mikhail <mmurzin1996@mail.ru>
2019-12-10 00:26:42 +0500

    Add application.yml

--- src/main/java/sia/tacocloud/controller/OrderController.java
+++ src/main/java/sia/tacocloud/controller/OrderController.java
@@ -1,8 +1,11 @@
 package sia.tacocloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import sia.tacocloud.domain.Order;
+import sia.tacocloud.domain.OrderProps;
 import sia.tacocloud.domain.User;
 import sia.tacocloud.repository.OrderRepository;
 
@@ -21,9 +25,12 @@ import javax.validation.Valid;
 @SessionAttributes("order")
 public class OrderController {
     private OrderRepository orderRepo;
+    private OrderProps props;
 
-    public OrderController(OrderRepository orderRepo) {
+    public OrderController(OrderRepository orderRepo, OrderProps props)
+    {
         this.orderRepo = orderRepo;
+        this.props = props;
     }
 
     @GetMapping("/current")
@@ -41,5 +48,14 @@ public class OrderController {
         orderRepo.save(order);
         sessionStatus.setComplete();
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String ordersForUser(
+            @AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, props.getPageSize());
+        model.addAttribute("orders",
+                orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
     }
 }
