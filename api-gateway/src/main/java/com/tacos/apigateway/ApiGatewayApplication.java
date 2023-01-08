d061d14e7b4ad9d46602837cba7daab1d73361dd
mikhail <mmurzin1996@gmail.com>
2023-01-08 20:09:41 +0500

    Migrate api-gateway service from Zuul to Cloud-native Gateway

--- api-gateway/src/main/java/com/tacos/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/tacos/apigateway/ApiGatewayApplication.java
@@ -2,9 +2,7 @@ package com.tacos.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayApplication {
 
